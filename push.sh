b817030982e481c4dfa77d6b68b2f2de05cfa4d7
Adriano Porcheddu <adriano@porcheddu.net>
2022-01-15 12:04:27 +0100

    Update

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git remote set-url origin https://github.com/adrianus07/adrianus07.github.io.git 
+git remote set-url origin git@github.com:adrianus07/adrianus07.github.io.git  
 git add .
 git commit -m "Update"
 git push
